f7783440d993fc8ee6ca7af6a720ff6d87bcc26a
web-cubr <mishaoboronov@yandex.ru>
2020-08-23 18:50:26 +0300

    fix

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -11,24 +11,9 @@ var forms = {
 			clearMaskOnLostFocus: true,
 			clearIncomplete: true,
 		});
-		
-		var creditCard = new Inputmask({
-			mask: "9999-9999-9999-9999"
-		});
-		
-		var expiration = new Inputmask({
-			mask: "99/99"
-		});
-		
-		var cvv = new Inputmask({
-			mask: "999"
-		});
 
-		//im.mask(selector);
+		im.mask(selector);
 		
-		creditCard.mask('.js-credit-card');
-		expiration.mask('.js-expiration');
-		cvv.mask('.js-cvv');
 	},
 
 	validate: () => {
@@ -59,18 +44,6 @@ var forms = {
 						sucsess(thank);
 					}
 					
-					if ( $(form).hasClass('order__form') ) {
-			
-						$('.order__section-inside:visible').closest('.order__section').addClass('is-complete is-sucsess').next().removeClass('is-closed');
-						
-					}
-					
-					if ( $(form).hasClass('order__returing') ) {
-						
-						$('.order__returing-step:visible').not('.order__returing-step:last-child').hide().next().fadeIn(500);
-						
-					}
-					
 					$.ajax({
 						type: "POST",
 						url: $(form).attr("action"),
@@ -81,7 +54,7 @@ var forms = {
 					});
 				},
 				rules: {
-					email: {
+					phone: {
 						required: true,
 						minlength: 5,
 					},
@@ -125,62 +98,23 @@ var forms = {
 				if ($input.val() == "") $input.parent().removeClass("is-focus");
 			});
 		
-		$('.order__head').click(function(){
-			let parent = $(this).closest('.order__section');
-			
-			if ( parent.hasClass('is-complete') ) {
-				parent.toggleClass('is-sucsess');
-			}
-			
+		/* placeholder*/       
+		$('input, textarea').each(function(){
+			var placeholder = $(this).attr('placeholder');
+			$(this).focus(function(){ $(this).attr('placeholder', '');});
+			$(this).focusout(function(){             
+				$(this).attr('placeholder', placeholder);           
+			});
 		});
+		/* placeholder*/
 		
-		$('.js-pets-add').click(function(){
+		$('.js-file').change(function(){
 			
-			let name = $('.js-pets-name').val();
+			let fileName = $(this).val().split('\\');
 			
-			$('.js-pets-container').prepend(`<div class="order__column order__column_small">
-                                                <div class="order__pet">
-                                                    <div class="order__pet-icon">
-                                                        <svg class="icon icon-paw" viewBox="0 0 23 23">
-                                                            <use xlink:href="/app/icons/sprite.svg#paw"></use>
-                                                        </svg>
-                                                    </div>
-                                                    <div class="order__pet-text p">${name}</div>
-                                                    <div class="order__pet-delete">
-                                                        <svg class="icon icon-close" viewBox="0 0 17 17">
-                                                            <use xlink:href="/app/icons/sprite.svg#close"></use>
-                                                        </svg>
-                                                    </div>
-                                                </div>
-                                            </div>`)
+			$(this).closest('.js-file-container').find('.js-file-name').text(fileName[fileName.length - 1]);
 			
-		});
-		
-		$(document).on("click", ".order__pet-delete", function() {
-			$(this).closest('.order__column').remove();
-		});
-		
-		$('.js-customer').change(function(){
-			
-			if ( $(this).val() == 'old' ) {
-				$('.order__section').hide();
-				$('.order__returing').fadeIn(500);
-			} else {
-				$('.order__returing').hide();
-				$('.order__section').fadeIn(500);
-			}
-			
-		});
-		
-		$('.js-show-adress').change(function(){
-			
-			$('.order__section_adress').fadeToggle(500);
-			
-		});
-		
-		$('.js-dublicate-adress').change(function(){
-			
-			$('.js-billing-adress').fadeToggle(300);
+			console.log('add');
 			
 		});
 		
